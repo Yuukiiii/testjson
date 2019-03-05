5c6a1a5fa5cc477cb86de8e0aa863759460f26c0
yuukiiii <yuukiiii@qq.com>
2019-03-05 17:48:10 +0800

    test

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"./proto"
+	"baseservice/testjson/proto"
 	"fmt"
 	"github.com/golang/protobuf/proto"
 )
 
-func main()  {
+func main() {
 	h1 := &head.Head{
-		MsgTypeName:string("test"),
-		Ret:int32(1),
-		Key:string("key"),
+		MsgTypeName: string("test"),
+		Ret:         int32(1),
+		Key:         string("key"),
 	}
 	proto.Marshal(h1)
 	fmt.Println(h1.String())
