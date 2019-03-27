57318fb08f519412afac6a53c956f2f103b7681d
Yuukiiii <yuukiiiichan@gmail.com>
2019-03-27 11:04:46 +0800

    test log.critical

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"common/util"
 	"fmt"
+	log "github.com/cihub/seelog"
 )
 
 
 func main() {
-	var gcf Config
-	if err := util.ParseConf(&gcf, "etc/country_code.yaml"); err != nil {
-		return
-	}
-
-	var c = gcf.Country
-	fmt.Println(c["日本"])
-	fmt.Println(c["美国"])
-
+	log.Criticalf("ParseConf err")
+	fmt.Println(123)
 }
